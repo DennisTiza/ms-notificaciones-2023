20adde76cc6e504445c2ece9192b4a11589f9a05
DennisTiza <dennis.tisalema30646@ucaldas.edu.co>
2023-10-18 18:41:01 -0500

    Creacion de Notificaciones via correo electronico Sengrid

---
+++ Controllers/NotificacionesController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using ms_notificaciones.Models;
+
+namespace ms_notificaciones.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class NotificacionesController : ControllerBase
+{
+    [Route("correo")]
+    [HttpPost]
+    public async Task<ActionResult> EnviarCorreo(ModeloCorreo datos){
+
+        var apikey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apikey);
+        var from = new EmailAddress("jonathan.cadena28906@ucaldas.edu.co","Dennis Tisalema");
+        var subject = datos.asuntoCorreo;
+        var to = new EmailAddress(datos.correoDestino, datos.nombreDestino);
+        var plainTextContent = "plain text content";
+        var htmlContent = datos.contenidoCorreo;
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        var response = await client.SendEmailAsync(msg);
+        if(response.StatusCode == System.Net.HttpStatusCode.Accepted){
+            return Ok("Correo enviado a la dirección: " + datos.correoDestino);
+        }else{
+            return BadRequest("Error al enviar el mensaje a la dirección: " + datos.correoDestino);
+        }
+    }
+
+}
